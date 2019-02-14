22e0b74d8948fd894070eced6ebb4c6b4502c632
Van Maercke Christophe <vanmaerckechri@gmail.com>
2019-02-14 14:50:42 +0100

    add init game board class

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,11 +30,82 @@ document.addEventListener("DOMContentLoaded", function(event)
 		}
 	}
 
+	class GameBoard
+	{
+		constructor(canvas, rowsLength, colsLength)
+		{
+			this.spriteSize = 0;
+
+			this.canvas = canvas;
+			this.ctx = canvas.getContext("2d");
+			this.rowsLength = rowsLength;
+			this.colsLength = colsLength;
+
+			this.init();
+  		}
+
+  		drawBoard()
+  		{
+  			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+  			for (let r = 0, rLength = this.rowsLength; r < rLength; r++)
+  			{
+  				let posY = this.spriteSize * r;
+  				for (let c = 0, cLength = this.colsLength; c < cLength; c++)
+  				{
+  					let posX = this.spriteSize * c;
+					this.ctx.rect(posX, posY, this.spriteSize, this.spriteSize);
+					this.ctx.stroke();
+  				}
+  			}
+  		}
+
+  		updateCanvasSize()
+  		{
+  			// rest canvas container size
+  			this.canvas.width = 0;
+  			this.canvas.height = 0;
+  			
+  			let canvasContainerSize;
+  			let canvasContainerWidth = this.canvas.parentNode.offsetWidth - '20';
+  			let canvasContainerHeight = this.canvas.parentNode.offsetHeight - '20';;
+
+  			// adapt to portrait or landscape
+  			if (canvasContainerHeight > canvasContainerWidth)
+  			{
+  			  	canvasContainerSize = canvasContainerWidth;
+  				this.spriteSize = Math.floor(canvasContainerSize / this.colsLength);	
+  			}
+  			else
+  			{
+	  			canvasContainerSize = canvasContainerHeight;
+	  			this.spriteSize = Math.floor(canvasContainerSize / this.rowsLength);
+  			}
+
+  			this.canvas.width = canvasContainerSize;
+  			this.canvas.height = canvasContainerSize;
+
+  			this.drawBoard();
+  		}
+
+  		init()
+  		{
+  			this.updateCanvasSize();
+  			this.drawBoard();
+
+  			window.addEventListener('resize', () => { this.updateCanvasSize(); }, false);
+  		}
+	}
+
 	class Engine
 	{
 		static init()
 		{
-			CodeLines.init()
+			//
+			let canvas = document.getElementById('game-canvas');
+			let gameBoard = new GameBoard(canvas, 8, 8);
+			//
+			CodeLines.init();
 		}
 	}
 
